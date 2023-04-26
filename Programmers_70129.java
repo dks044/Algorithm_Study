079d0bda3dd26801ef7f91d5a1238ddb9cc58f0b
AJN <74749417+dks044@users.noreply.github.com>
2023-04-26 17:36:41 +0900

    Create Programmers_70129.java

---
+++ Programmers_70129.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(s.length() !=1){
+            char[] ch = s.toCharArray();
+            ArrayList<Character> ch_arr = new ArrayList<>();
+            for(int i=0;i<ch.length;i++){
+                ch_arr.add(ch[i]);
+            }
+
+            for(int i=0;i<ch_arr.size();i++){
+                if(ch_arr.get(i) == '0'){
+                    ch_arr.remove(i);
+                    answer[1] +=1;
+                    i--;
+                }
+            }
+            s = Integer.toBinaryString(ch_arr.size());//ex)110
+            System.out.println(s);//0뺴고 남은 문자열길이 이진수
+            answer[0] +=1;   
+        }
+        
+        
+        
+        return answer;
+    }
+}
