9d1583af538676e66ade3f9ac9bbf5d1549972f2
AJN <74749417+dks044@users.noreply.github.com>
2023-06-15 14:16:57 +0900

    행렬의 곱셈 (Lv.2, 수학)

--- Programmers12949.java
+++ Programmers12949.java
@@ -2,6 +2,7 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
         
+        //배열1의 열인덱스 x 배열2의 행 인덱스
         //i는 행, j는 열, k를 반복하며 순회해서 행렬곱셈 구현 
         for(int i=0; i<arr1.length;i++){
             for(int j=0;j<arr2[0].length;j++){
