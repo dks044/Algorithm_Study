5f2f22e50739caf7ca83e1ee81fc12108fb6fd64
AJN <74749417+dks044@users.noreply.github.com>
2023-10-17 18:32:37 +0900

    정수 제곱근 판별 (Lv.1)

---
+++ Programmers12934.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        if(Math.sqrt(n) %1 !=0) answer = -1;
+        else{
+            answer = (long)Math.sqrt(n)+1;
+            answer *= answer;
+        }
+        return answer;
+    }
+}
