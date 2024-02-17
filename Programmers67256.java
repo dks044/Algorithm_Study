3da539d7e6e8d2844b27a8c2d512694d32604d44
AJN <74749417+dks044@users.noreply.github.com>
2024-02-17 17:40:44 +0900

    키패드 (Lv.1) - 리팩토링

--- Programmers67256.java
+++ Programmers67256.java
@@ -1,85 +1,71 @@
 import java.util.*;
-class Point{
+class Point {
     int x;
     int y;
     Point(int x,int y){
         this.x = x;
         this.y = y;
     }
-    public int getX(){
-        return this.x;
-    }
-    public int getY(){
-        return this.y;
-    }
-    
 }
-
+// am 2:17 시작
 class Solution {
-    
-    static String getLR(double leftabs,double rightabs,String hand){
-        if(hand.equals("right") && leftabs == rightabs) return "R";
-        if(hand.equals("left") && leftabs == rightabs) return "L";
-        if(leftabs < rightabs) return "L";
-        if(leftabs > rightabs) return "R";
+    List<Integer> leftNumber = List.of(1,4,7);
+    List<Integer> rightNumber = List.of(3,6,9);
         
-        return "";
-    }
-    
-static double getAbs(Point point, int index, List<Point> p_list) {
-    Point middle = p_list.get(index);
-
-    int x = point.getX();
-    int y = point.getY();
-    int x1 = middle.getX();
-    int y1 = middle.getY();
-
-    return Math.abs(x1 - x) + Math.abs(y1 - y); // 맨해튼 거리 계산
-}
-    
     public String solution(int[] numbers, String hand) {
-        StringBuilder answer = new StringBuilder();
-        List<Point> p_list = new ArrayList<>();
-        for(int i=0;i<4;i++){
+        int indexIng = 1;//10(*), 11(0), 12(#)
+        Map<Integer,Point> maps = new HashMap<>();
+        for(int i=0; i<4;i++){
             for(int j=0;j<3;j++){
-                p_list.add(new Point(i,j));
+                if(indexIng == 11) maps.put(0,new Point(i,j));
+                 maps.put(indexIng++,new Point(i,j));
             }
         }
-        
-        List<Integer> left = List.of(1,4,7);
-        List<Integer> right = List.of(3,6,9);
-        List<Integer> middle = List.of(2,5,8,0);
-            
-        Stack<Point> leftHand = new Stack<>();
-        Stack<Point> rightHand = new Stack<>();
-        //왼손 오른손 초기위치 할당
-        leftHand.push(new Point(3,0));
-        rightHand.push(new Point(3,2));
+        Point leftHand = maps.get(10); //왼손초기위치 *
+        Point rightHand = maps.get(12); //오른손초기위치 #
+        StringBuilder result = new StringBuilder();
         
         for(int i=0;i<numbers.length;i++){
-           int index = (numbers[i] == 0) ? 10 : numbers[i] - 1;
-            Point currPoint = p_list.get(index);
-
-            if(left.contains(numbers[i])){
-                answer.append("L");
-                leftHand.push(currPoint);
-            } else if(right.contains(numbers[i])){
-                answer.append("R");
-                rightHand.push(currPoint);
-            } else {
-                double leftabs = getAbs(leftHand.peek(), index, p_list);
-                double rightabs = getAbs(rightHand.peek(), index, p_list);
-                String chosenHand = getLR(leftabs, rightabs, hand);
-
-                if(chosenHand.equals("L")){
-                    leftHand.push(currPoint);
-                } else {
-                    rightHand.push(currPoint);
-                }
-                answer.append(chosenHand);
+            if(leftNumber.contains(numbers[i])){
+                result.append("L");
+                leftHand = maps.get(numbers[i]);
+                continue;
+            }
+            if(rightNumber.contains(numbers[i])){
+                result.append("R");
+                rightHand = maps.get(numbers[i]);
+                continue;
+            }
+            Point p = maps.get(numbers[i]);
+            int leftDistance = manDistance(leftHand,p);
+            int rightDistance = manDistance(rightHand,p);
+            //두손 거리가 같을경우(주손잡이의 따라 나눔)
+            if(leftDistance == rightDistance && hand.equals("left")){
+                result.append("L");
+                leftHand = maps.get(numbers[i]);
+            }
+            if(leftDistance == rightDistance && hand.equals("right")){
+                result.append("R");
+                rightHand = maps.get(numbers[i]);
+            }
+            //왼손거리가 더 가까운경우
+            if(leftDistance < rightDistance){
+                result.append("L");
+                leftHand = maps.get(numbers[i]);
+            }
+            //오른손거리가 더 가까운경우
+            if(leftDistance > rightDistance){
+                result.append("R");
+                rightHand = maps.get(numbers[i]);
             }
         }
-            
-        return answer.toString();
+        
+        
+        return result.toString();
     }
+    public static int manDistance(Point hand,Point p){
+        int distance = Math.abs(hand.x - p.x) + Math.abs(hand.y - p.y);
+        return distance;
+    }
+    
 }
