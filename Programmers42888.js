8639ed262c1e47757137d06d60cdbbacbf27fde9
AJN <74749417+dks044@users.noreply.github.com>
2024-07-16 01:20:58 +0900

    오픈채팅방 (Lv2, 해시) -복습

--- Programmers42888.js
+++ Programmers42888.js
@@ -4,36 +4,21 @@ const c = 'Change';
 
 function solution(record) {
     var answer = [];
-    var users = new Map();
-    for(let i =0;i<record.length;i++){
-        let r = record[i].split(' ');
-        let action = r[0];
-        let uid = r[1];
-        let nickName = r[2];
-        //입장일경우
-        if(action === e){
-            users.set(uid,nickName);
-        }
-        
-        //바꿀경우
-        if(action === c){
-            users.set(uid,nickName);
+    const maps = new Map();
+    for(let r of record){
+        let [action, uid, nickname] = r.split(" ");
+        if(action === e || action === c){
+            maps.set(uid,nickname);
         }
     }
-    //채팅방 로직 시작
-    for(let i =0;i<record.length;i++){
-        let r = record[i].split(' ');
-        let action = r[0];
-        let nickname = users.get(r[1]);
-        //입장일경우
+    for(let r of record){
+        let [action, uid, nickname] = r.split(" ");
         if(action === e){
-            answer.push(nickname+'님이 들어왔습니다.');
+            answer.push(maps.get(uid)+'님이 들어왔습니다.');
         }
-        //나갈경우
         if(action === l){
-            answer.push(nickname+'님이 나갔습니다.');
+            answer.push(maps.get(uid)+'님이 나갔습니다.');
         }
-
     }
     
     return answer;
