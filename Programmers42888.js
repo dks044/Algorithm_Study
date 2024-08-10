d8dddc61ee6eff2d0952fa33e064c7ee9d4ee84c
AJN <74749417+dks044@users.noreply.github.com>
2024-08-10 21:35:18 +0900

    오픈채팅방 (Lv2, 해시)

--- Programmers42888.js
+++ Programmers42888.js
@@ -4,21 +4,15 @@ const c = 'Change';
 
 function solution(record) {
     var answer = [];
-    const maps = new Map();
-    for(let r of record){
-        let [action, uid, nickname] = r.split(" ");
-        if(action === e || action === c){
-            maps.set(uid,nickname);
-        }
+    var map = new Map();
+    for(let sentence of record){
+        let [action,uid,nickname] = sentence.split(" ");
+        if(action === e || action === c) map.set(uid,nickname);
     }
-    for(let r of record){
-        let [action, uid, nickname] = r.split(" ");
-        if(action === e){
-            answer.push(maps.get(uid)+'님이 들어왔습니다.');
-        }
-        if(action === l){
-            answer.push(maps.get(uid)+'님이 나갔습니다.');
-        }
+    for(let sentence of record){
+        let [action,uid,nickname] = sentence.split(" ");
+        if(action === e) answer.push(map.get(uid)+'님이 들어왔습니다.');
+        if(action === l) answer.push(map.get(uid)+'님이 나갔습니다.');
     }
     
     return answer;
