8637958716076c7e8126597b83fedfb3dd01fb22
AJN <74749417+dks044@users.noreply.github.com>
2024-10-24 02:34:09 +0900

    프로세스 (Lv.2, 큐) - 복습 1회

--- Programmers42587.java
+++ Programmers42587.java
@@ -1,36 +1,41 @@
 import java.util.*;
-class Work{
-    int price;
-    int location;
-    Work(int price, int location){
-        this.price = price;
-        this.location = location;
+class Process {
+    int index;
+    int priority;
+    Process(int index, int priority) {
+        this.index = index;
+        this.priority = priority;
     }
 }
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-    	
-        Queue<Work> q = new LinkedList<>();
+        List<Process> list = new ArrayList<>();
+        
+        Queue<Process> q = new LinkedList<>();
         for(int i=0;i<priorities.length;i++){
-            q.add(new Work(priorities[i],i));
+            Process p = new Process(i,priorities[i]);
+            q.add(p);
         }
-        ArrayList<Work> ssibal_arr = new ArrayList<>();
+        int play = 1;
         while(!q.isEmpty()){
-            Work ssibal = q.poll();
-            
-            int max_price =0;
-            for(Work w : q) if(w.price > max_price) max_price = w.price;
-            if(max_price > ssibal.price) q.add(ssibal);
-            else ssibal_arr.add(ssibal);
+            Process current = q.poll();
+            boolean isFind = false;
+            for(Process p : q){
+                if(p.priority > current.priority){
+                    isFind = true;
+                }
+            }
+            if(isFind) q.add(current);
+            if(!isFind){
+                if(location == current.index){
+                    return play;
+                }
+                play++;
+            }
         }
-        for(Work w : ssibal_arr){
-            if(w.location == location){
-                answer++;
-                break;
-            }else answer++;
-        }
+        
         
         return answer;
     }
