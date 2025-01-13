13a06b7a834ca489d343470c93642a83536404e0
AJN <buzz781@gmail.com>
2025-01-14 02:02:14 +0900

    수정정

--- programmers12932.java
+++ programmers12932.java
@@ -1,29 +1,33 @@
 import java.util.*;
 class Solution {
-    //배열뒤집기
-    public void reverseArr(int [] arr){
-        int length = arr.length;
-        for(int i=0;i<length/2;i++){
-            int temp = arr[i];
-            arr[i] = arr[length-i-1];
-            arr[length-i-1] = temp;
-        }
+    public static void reverseArray(int[] arr) {
+    int length = arr.length;
+    for (int i = 0; i < length / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[length - i - 1];
+        arr[length - i - 1] = temp;
     }
-    //배열만들기
-    public int[] createArr(long n){
-        String int_temp = String.valueOf(n);
-        char[] int_tempToArr = int_temp.toCharArray();
-        int[] create_arr = new int[int_tempToArr.length];
-        for(int i=0;i<int_tempToArr.length;i++){
-            create_arr[i] = int_tempToArr[i] - '0';
-        }
-        return create_arr;
-    }
-    
-    
+}
+
     public int[] solution(long n) {
-        int[] answer = createArr(n);
-        reverseArr(answer);
+        
+        String num = String.valueOf(n);
+        int[] answer = new int[num.length()];
+        char[] numm = num.toCharArray();
+        ArrayList<Character> num_arr = new ArrayList<>();
+        for(char word : numm){
+            num_arr.add(word);
+        }
+        ArrayList<Integer> num_arr2 = new ArrayList<>();
+        for(int i=0;i<num_arr.size();i++){
+            Integer a = Character.valueOf(num_arr.get(i))-'0';
+            num_arr2.add(a);
+        }
+        for(int i=0;i<num_arr2.size();i++){
+            answer[i] = num_arr2.get(i);
+        }
+        reverseArray(answer);
+        
         return answer;
     }
 }
