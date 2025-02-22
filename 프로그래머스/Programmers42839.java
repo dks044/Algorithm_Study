a8c6f014396a80048f971dd2fb910b501b07cddb
AJN <74749417+dks044@users.noreply.github.com>
2025-02-23 01:34:17 +0900

    소수찾기.java

--- 프로그래머스/Programmers42839.java
+++ 프로그래머스/Programmers42839.java
@@ -1,55 +1,43 @@
-import java.util.HashSet;
-import java.util.Set;
- 
-public class 소수찾기 {
-    static Set<Integer> set;
-    static boolean[] visited = new boolean[7]; // numbers는 길이 1 이상 7 이하인 문자열
- 
-    public static void main(String[] args) {
-        System.out.println(solution("17"));
-        System.out.println(solution("011"));
-    }
- 
-    public static int solution(String numbers) {
-        int answer = 0;
-        set = new HashSet<>();
-        dfs(numbers, "", 0);
- 
-        for (Integer num : set) {
-            if (isPrime(num)) {
-                answer++;
-            }
+import java.util.*;
+class Solution {
+    //Optional
+    static int answer = 0;
+    static Set<Integer> set = new HashSet<>();
+    static boolean[] visited;
+    
+    public int solution(String numbers) {
+        //틀
+        visited = new boolean[numbers.length()];
+        
+        dfs(0,numbers,"");
+
+        for(int i : set){
+            System.out.println(i);
+            if(isSoju(i)) answer++;
         }
+        
         return answer;
     }
- 
-    public static void dfs(String numbers, String s, int depth) {
-		// numbers 의 끝까지 다 탐색했다면 종료
-        if (depth > numbers.length()) {
+    static void dfs(int depth,String numbers, String temp){
+        if(depth > numbers.length()){
             return;
         }
- 
-        for (int i = 0; i < numbers.length(); i++) {
-            if(!visited[i]) {
+        for(int i=0;i<numbers.length();i++){
+            if(!visited[i]){
                 visited[i] = true;
-                set.add(Integer.parseInt(s + numbers.charAt(i)));
-                dfs(numbers ,s + numbers.charAt(i), depth + 1);
+                set.add(Integer.parseInt(temp + numbers.charAt(i)));
+                dfs(depth + 1,numbers,temp + numbers.charAt(i));
                 visited[i] = false;
             }
         }
     }
- 
-    public static boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
+    
+    //소수인지 구하는새끼
+    static boolean isSoju(int number){
+        if(number < 2) return false;
+        for(int i=2;i<=Math.sqrt(number);i++){
+            if(number % i ==0) return false;
         }
-		// 에라토스테네스 체
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
- 
         return true;
     }
 }
