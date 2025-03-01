ae85f1bc4fc9c8cd0535db40a9e319e2f65215ae
AJN <74749417+dks044@users.noreply.github.com>
2025-03-02 02:28:23 +0900

    소수찾기 (Lv.2, 완전탐색&백트래킹 , JAVA) - 복습 3회

--- 프로그래머스/소수찾기.java
+++ 프로그래머스/소수찾기.java
@@ -1,23 +1,22 @@
 import java.util.*;
-
 class Solution {
     static int answer = 0;
-    static boolean[] visited = new boolean[7];
     static Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
-        dfs(numbers,0,"");
+        boolean[] visited = new boolean[numbers.length()];
+        dfs(numbers,"",visited);
         for(int i : set){
             if(isPrime(i)) answer++;
         }
         return answer;
     }
-    static void dfs(String numbers, int depth, String temp){
+    static void dfs(String numbers, String temp, boolean[] visited){
         for(int i=0;i<numbers.length();i++){
             if(!visited[i]){
                 visited[i] = true;
                 set.add(Integer.parseInt(temp + numbers.charAt(i)));
-                dfs(numbers,depth + 1, temp + numbers.charAt(i));
+                dfs(numbers, temp + numbers.charAt(i),visited);
                 visited[i] = false;
             }
         }
