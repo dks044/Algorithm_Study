b8dc740831ebbcdba2eb23d977282505a0e53aac
AJN <74749417+dks044@users.noreply.github.com>
2025-03-18 20:33:47 +0900

    소수찾기 (Lv.2, 완전탐색&백트래킹 , JAVA) - 복습 4회

--- 프로그래머스/소수찾기.java
+++ 프로그래머스/소수찾기.java
@@ -2,29 +2,28 @@ import java.util.*;
 class Solution {
     static int answer = 0;
     static Set<Integer> set = new HashSet<>();
-    
+    static boolean[] visited = new boolean[7];
     public int solution(String numbers) {
-        boolean[] visited = new boolean[numbers.length()];
-        dfs(numbers,"",visited);
+        dfs(numbers,"");
         for(int i : set){
             if(isPrime(i)) answer++;
         }
         return answer;
     }
-    static void dfs(String numbers, String temp, boolean[] visited){
+    static void dfs(String numbers, String temp){
         for(int i=0;i<numbers.length();i++){
             if(!visited[i]){
                 visited[i] = true;
                 set.add(Integer.parseInt(temp + numbers.charAt(i)));
-                dfs(numbers, temp + numbers.charAt(i),visited);
+                dfs(numbers, temp + numbers.charAt(i));
                 visited[i] = false;
             }
         }
     }
-    static boolean isPrime(int n){
-        if(n < 2) return false;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n % i == 0) return false;
+    static boolean isPrime(int number){
+        if(number < 2) return false;
+        for(int i=2;i<=Math.sqrt(number);i++){
+            if(number % i ==0) return false;
         }
         return true;
     }
