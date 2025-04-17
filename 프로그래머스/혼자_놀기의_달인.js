f0fcb13158001993413a81742c08ac3f20d8d64d
AJN <74749417+dks044@users.noreply.github.com>
2025-04-17 17:59:33 +0900

    혼자 놀기의 달인 (Lv.2, 백트래킹, JS) - 복습횟수 5회

--- 프로그래머스/혼자_놀기의_달인.js
+++ 프로그래머스/혼자_놀기의_달인.js
@@ -1,20 +1,17 @@
 function solution(cards) {
-    var answer = 0;
     var visited = Array(cards.length).fill(false);
-    
     var groupsize = [];
     for(let i=0;i<cards.length;i++){
-        let idx = i;
-        let count =0;
-        while(!visited[idx]){
-            visited[idx] = true;
-            idx = cards[idx] -1;
+        let index = i;
+        let count = 0;
+        while(!visited[index]){
+            visited[index] = true;
+            index = cards[index] -1;
             count++;
         }
         groupsize.push(count);
     }
-    if(groupsize.length == 1) return 0;
-    groupsize.sort((a,b)=> b-a);
-    
-    return groupsize[0] * groupsize[1]; 
+    groupsize.sort((a,b)=>b - a);
+    if(groupsize.length ==0) return 0;
+    return groupsize[0] * groupsize[1];
 }
