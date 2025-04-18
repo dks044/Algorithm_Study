5628573f7a55e2b20efeaee1d1e32d0ee46a809f
AJN <74749417+dks044@users.noreply.github.com>
2025-04-18 15:52:16 +0900

    롤케이크_자르기 (Lv.2, 해시, JS) - 복습횟수 1

--- 프로그래머스/롤케이크_자르기.js
+++ 프로그래머스/롤케이크_자르기.js
@@ -1,31 +1,24 @@
 function solution(topping) {
-    let answer = 0;
-
-    const left = new Map();   // 철수 쪽
-    const right = new Map();  // 동생 쪽
-
-    // 처음엔 전부 동생 쪽에 있음
-    for (let t of topping) {
-        right.set(t, (right.get(t) || 0) + 1);
+    var answer = 0;
+    var big_brohter = new Map();
+    var brother = new Map();
+    //동생먼저 다 가져가라ㅋㅋ
+    for(let t of topping){
+        brother.set(t,(brother.get(t) || 0) +1);
     }
-
-    for (let i = 0; i < topping.length - 1; i++) {
-        const t = topping[i];
-
-        // 철수 쪽에 추가
-        left.set(t, (left.get(t) || 0) + 1);
-
-        // 동생 쪽에서 제거
-        right.set(t, right.get(t) - 1);
-        if (right.get(t) === 0) {
-            right.delete(t);
+    for(let i=0;i<topping.length;i++){
+        let unit = topping[i];
+        //형 한테 주기
+        big_brohter.set(unit,(big_brohter.get(unit) || 0) +1);
+        
+        //동생 제거
+        brother.set(unit, brother.get(unit) - 1);
+        if(brother.get(unit) ===0){
+            brother.delete(unit);
         }
-
-        // 종류 수 비교
-        if (left.size === right.size) {
-            answer++;
-        }
+        if(brother.size === big_brohter.size) answer++;
     }
-
+    
+    
     return answer;
 }
