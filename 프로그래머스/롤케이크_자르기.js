48492bc3ceb2a4e4116ff4c753bc0a2e16f6b4e4
AJN <74749417+dks044@users.noreply.github.com>
2025-05-01 22:20:54 +0900

    롤케이크_자르기 (Lv.2, 해시, JS) - 복습횟수 3

--- 프로그래머스/롤케이크_자르기.js
+++ 프로그래머스/롤케이크_자르기.js
@@ -1,23 +1,19 @@
 function solution(topping) {
     var answer = 0;
-    const big_brother = new Map();
+    const bigBrother = new Map();
     const brother = new Map();
     for(let t of topping){
-        brother.set(t, (brother.get(t) || 0)+1 );
+        brother.set(t, (brother.get(t) || 0) + 1 );
     }
     for(let t of topping){
-        big_brother.set(t, (big_brother.get(t) || 0)+1 );
+        bigBrother.set(t, (bigBrother.get(t) || 0) + 1 );
         
-        brother.set(t, brother.get(t) - 1);
-        if(brother.get(t) === 0){
+        brother.set(t,(brother.get(t) || 0) -1 );
+        if(brother.get(t) <= 0){
             brother.delete(t);
         }
-        
-        if(big_brother.size === brother.size){
-            answer++;
-        }
+        if(bigBrother.size === brother.size) answer++;
     }
-    
     
     return answer;
 }
