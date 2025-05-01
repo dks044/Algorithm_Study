65982e8b0196523ba7e414f9d1baeb667b46eda2
AJN <74749417+dks044@users.noreply.github.com>
2025-05-01 22:31:40 +0900

    마법의_엘리베이터 (Lv.2, 그리디, JS) - 복습횟수 3

--- 프로그래머스/마법의_엘리베이터.js
+++ 프로그래머스/마법의_엘리베이터.js
@@ -1,17 +1,15 @@
 function solution(storey) {
-    var answer = 0;
-    while(storey > 0){
+    let answer = 0;
+    while(storey >0){
         let current = storey % 10;
-        let next = Math.floor( (storey % 100) / 10 );
-        if(current > 5 || (next >=5 && current === 5)){
-            answer += 10 - current;
-            storey += 10 - current;
+        let next = Math.floor( storey % 100 / 10 )
+        if(current > 5 || (current === 5 && next >= 5) ){
+            answer += 10-current;
+            storey += 10-current;
         }else{
             answer += current;
         }
-        storey = Math.floor(storey / 10);
+        storey = Math.floor( storey / 10 );
     }
-        
-        
     return answer;
 }
