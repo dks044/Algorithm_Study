60f45b31842e1dcb3f3fc03584d0c4f1d9934f69
AJN <74749417+dks044@users.noreply.github.com>
2025-05-14 13:17:33 +0900

    땅따먹기 (Lv2, DP, JS) - 복습횟수 1회

--- 프로그래머스/땅따먹기.js
+++ 프로그래머스/땅따먹기.js
@@ -1,16 +1,11 @@
 function solution(land) {
     var answer = 0;
-    const dp = [];
+    const dp = [...land];
     
-    //dp 테이블 ㄱㄱ
-    for(let i=0;i<land.length;i++){
-        dp[i] = land[i].slice();
-    }
-
     for(let i=1;i<land.length;i++){
         for(let j=0;j<4;j++){
-            const prev = dp[i-1].filter((value,index) => index !== j);
-            dp[i][j] = Math.max(...prev) + land[i][j];
+            const prev = land[i-1].filter((v,index)=> index !== j);
+            dp[i][j] = Math.max(...prev) + dp[i][j];
         }
     }
     
